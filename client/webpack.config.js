6b6d0c9c19996b6bb36b4b30e0d634ae08916eaa
darylbg <darylxcuf@gmail.com>
2023-06-18 15:14:18 +0100

    updated webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -23,8 +24,7 @@ module.exports = () => {
         template: './index.html',
         title: 'IndexedDB Demo'
       }),
-      new WorkboxPlugin.GenerateSW({}),
-      new WorkboxPlugin.InjectManifest({}),
+      new WorkboxPlugin.GenerateSW(),
     ],
 
     module: {
